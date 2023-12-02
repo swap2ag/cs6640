2b5d6805ebcac2e48cc3800f556f4eb070feb0c2
swap2ag <agarwal.swapnil640@gmail.com>
2023-12-03 01:30:38 +0530

    Update the paths for riscv toolchain for Ubuntu in WSL

--- env.sh
+++ env.sh
@@ -7,8 +7,8 @@ export EGOS=$(pwd)
 # export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-apple-darwin/bin
 
 # # if you use Ubuntu, you should uncomment the following two lines:
-# export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin
-# export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-linux-ubuntu14/bin
+export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-8.3.0-2020.04.1-x86_64-linux-ubuntu14/bin
+export PATH=$PATH:$EGOS/riscv-qemu-5.2.0-2020.12.0-preview1-x86_64-linux-ubuntu14/bin
 
 # # if you're using other OSes, please fill in XXX below
 # export PATH=$PATH:$EGOS/riscv64-unknown-elf-gcc-XXX/bin
